5d619ba9c4cfdcefa8a51538e9f5e47d60c0dc1a
Mike Doyle <mikedoyle007@gmail.com>
2018-08-25 19:29:49 -0700

    Add votedEvent

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -18,6 +18,11 @@ contract Election {
   // Store Candidates Count
   uint public candidatesCount;
 
+  // voted event
+  event votedEvent (
+    uint indexed _candidateId
+  );
+
   constructor() public {
     addCandidate("Candidate 1");
     addCandidate("Candidate 2");
@@ -40,5 +45,8 @@ contract Election {
 
     // update candidate vote count
     candidates[_candidateId].voteCount++;
+
+    // trigger voted event
+    emit votedEvent(_candidateId);
   }
 }
