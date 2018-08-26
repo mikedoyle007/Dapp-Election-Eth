eec18a051ceb3389049f7fbfbe6f12a22d19bdfb
Mike Doyle <mikedoyle007@gmail.com>
2018-08-25 18:27:25 -0700

    Truffle migration

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,9 @@ contract Election {
     uint voteCount;
   }
 
+  // Store accounts that have voted
+  mapping(address => bool) public voters;
+
   // Store Candidates
   // Fetch Candidate
   mapping(uint => Candidate) public candidates;
@@ -23,5 +26,19 @@ contract Election {
   function addCandidate(string _name) private {
     candidatesCount++;
     candidates[candidatesCount] =  Candidate(candidatesCount, _name, 0);
+  }
+
+  function vote(uint _candidateId) public {
+    // require that they haven't voted before
+    require(!voters[msg.sender]);
+
+    // require a valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+    // record that the voter has voted
+    voters[msg.sender] = true;
+
+    // update candidate vote count
+    candidates[_candidateId].voteCount++;
   }
 }
